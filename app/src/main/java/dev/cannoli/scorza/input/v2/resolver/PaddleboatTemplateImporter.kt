81caf85edca8041079a4bf0d201a8e7447fb087b
Brandon T. Kowalski <brandon@kowalski.io>
2026-04-26 23:01:38 -0400

    input v2: add PaddleboatTemplateImporter contract with no-op default

---
+++ app/src/main/java/dev/cannoli/scorza/input/v2/resolver/PaddleboatTemplateImporter.kt
@@ -0,0 +1,12 @@
+package dev.cannoli.scorza.input.v2.resolver
+
+import dev.cannoli.scorza.input.v2.ConnectedDevice
+import dev.cannoli.scorza.input.v2.DeviceTemplate
+
+interface PaddleboatTemplateImporter {
+    fun importFor(device: ConnectedDevice): DeviceTemplate?
+}
+
+class NoopPaddleboatTemplateImporter : PaddleboatTemplateImporter {
+    override fun importFor(device: ConnectedDevice): DeviceTemplate? = null
+}
