bcb7c5f182fc31b576cb5f2d80b878f8dfa12d16
Brandon T. Kowalski <brandon@kowalski.io>
2026-08-06 19:00:00 -0400

    app: stop building fonts in every process
    - the Application held an AppFonts field nothing read, which forced a 5.7 MB
      font parse in the emulator process as well as the launcher
    - MainActivity and SettingsViewModel already inject AppFonts themselves

--- app/src/main/java/dev/cannoli/scorza/CannoliApp.kt
+++ app/src/main/java/dev/cannoli/scorza/CannoliApp.kt
@@ -2,12 +2,9 @@ package dev.cannoli.scorza
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import dev.cannoli.scorza.di.AppFonts
-import javax.inject.Inject
 
 @HiltAndroidApp
 class CannoliApp : Application() {
-    @Inject lateinit var appFonts: AppFonts
 
     override fun attachBaseContext(base: android.content.Context) {
         super.attachBaseContext(dev.cannoli.scorza.i18n.LocaleOverride.wrap(base))
